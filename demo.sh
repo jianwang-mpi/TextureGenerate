10ece05cd00e83166492619f1c9490ebf3da66e2
JianWang <yt4766269@126.com>
2019-05-06 12:46:29 +0800

    debug

--- demo.sh
+++ demo.sh
@@ -3,5 +3,5 @@ model_path=pretrained_model/pretrained_weight.pkl
 
 rm -r example_results/texture
 mkdir example_results/texture
-
-python demo.py -m ${model_path} -i example_results/input -o example_results/texture
+# set the gpu id to 2
+python demo.py -g 2 -m ${model_path} -i example_results/input -o example_results/texture
